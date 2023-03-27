bf5043acd317d3a932c04dc322026c839fb0c7fd
Adam Tan <suwekforu@gmail.com>
2023-03-28 04:55:29 +0800

    new api link working

--- Project/my-pantry/src/pages/mypantry/mypantry.jsx
+++ Project/my-pantry/src/pages/mypantry/mypantry.jsx
@@ -7,12 +7,17 @@ import Checkbox from "../../components/checkbox/Checkbox";
 import InputBox from "../../components/inputbox/input";
 
 const MyPantry = () => {
-  const apikey = '&apiKey=7e512d08fbb14992a0d712854865b4eb'
-  const [Rec, secRec] = useState([])
-  const [url,seturl] = useState('https://api.spoonacular.com/recipes/findByIngredients?ingredients=+apples,+flour,+sugar,&apiKey=7e512d08fbb14992a0d712854865b4eb')
+  const apikey = '&apiKey=8b4379dc21bc4c1aa94cb4a62fdb130c'
+  
+// 7e512d08fbb14992a0d712854865b4eb
+// 4da76519f82242caa825c7db58bdf9f1
+// 8b4379dc21bc4c1aa94cb4a62fdb130c
+
+  const [Rec, setRec] = useState([])
+  const [url,seturl] = useState('https://api.spoonacular.com/recipes/findByIngredients?ingredients=+apples,+flour,+sugar,&number=2'+apikey)
 
   const [RecInfo, setRecInfo] = useState([])
-  const [RecipeInfoUrl,setRecipeInfoUrl] = useState('https://api.spoonacular.com/recipes/716429/information?includeNutrition=false')
+  const [RecipeInfoUrl,setRecipeInfoUrl] = useState('')
 
   const [value, setValue] = useState('');//search bar
 
@@ -20,17 +25,25 @@ const MyPantry = () => {
     setValue(event.target.value);
   }
 
+  function setfetchnavigate(recipeid) {
+    setRecipeInfoUrl('https://api.spoonacular.com/recipes/' + recipeid.toString() + '/information?includeNutrition=false' + apikey);
+    fetch('https://api.spoonacular.com/recipes/' + recipeid.toString() + '/information?includeNutrition=false' + apikey)
+    .then(response => response.json())
+    .then(json => window.open(json.sourceUrl, "_blank"));
+  }
+
     useEffect( () => {
       fetch(url)
       .then(response => response.json())
-      .then(json => secRec(json))
-    },[url],Rec)
+      .then(json => setRec(json))
+    },[url],)
 
-    useEffect( () => {
-      fetch(RecipeInfoUrl)
-      .then(response => response.json())
-      .then(json => setRecInfo(json))
-    },[RecipeInfoUrl],RecInfo)
+    // useEffect( () => {
+    //   fetch(RecipeInfoUrl)
+    //   .then(response => response.json())
+    //   .then(json => setRecInfo(json))
+    // },[RecipeInfoUrl])
+
 
   return (
     <div>
@@ -53,17 +66,11 @@ const MyPantry = () => {
           <div className="container">
           {Rec && Rec.map((item,index) =>(
             <div className ="item">
-              <div className ="item">
-              { () =>{setRecipeInfoUrl('https://api.spoonacular.com/recipes/'+Rec.id+'/information?includeNutrition=false'+apikey)
-                  fetch(RecipeInfoUrl)
-                  .then(response => response.json())
-                  .then(json => setRecInfo(json))}}
-              <h5>{item.title}</h5>
-              <a href={RecInfo.sourceUrl}>Click here for more information</a>
-              </div>
+              <h5 style={{ maxWidth: '150px', margin:'30px' }}>{item.title}</h5>             
               {/* <button style={{ width: "10px", height: "15px" }}></button> */}
-              <span onClick={() => console.log('you clicked!')}>
-              <img src={item.image} width ="100" height = "100"/>
+              <span onClick={() => setfetchnavigate(item.id)}
+              style={{display:"flex",justifyContent:"centre",width: "150px", height: "150px", objectFit: "cover"}}>
+              <img src={item.image} />
               </span>
               <Checkbox/>
             </div>
