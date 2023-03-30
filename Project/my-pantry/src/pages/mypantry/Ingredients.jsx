30fb9de68f96ed314f765b9fb50388450376d1a0
Adam Tan <suwekforu@gmail.com>
2023-03-31 05:40:58 +0800

    toggle buttons

--- Project/my-pantry/src/pages/mypantry/Ingredients.jsx
+++ Project/my-pantry/src/pages/mypantry/Ingredients.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./mypantry.scss";
+import "./ingredients.scss";
 import { collection, getDocs,getFirestore, docs, doc } from "firebase/firestore";
 
 const ShowIngredients = () => {
 
+    //to fetch from firestore
     const [ingredients, setIngredients] = useState([]);
     useEffect(() => {
         const fetchIngredients = async () => {
           const db = getFirestore();
-          const querySnapshot = await getDocs(collection(db, "test")); 
+          const querySnapshot = await getDocs(collection(db, "ingredients")); 
           //QuerySnapshot object contains an array of QueryDocumentSnapshot objects, each of which represents a single document in the collection or query results.
           const data = querySnapshot.docs.map((doc) => doc.data());
           setIngredients(data);
@@ -17,12 +19,29 @@ const ShowIngredients = () => {
         fetchIngredients();
       }, []);
 
+      //to toggle button
+      const [isOn, setIsOn] = useState(false);
+      const handleToggle = () => {
+        setIsOn(!isOn);
+      };
 
+      //button styling
+      const buttonStyle = {
+        color: 'black',
+        padding: '10px 20px',
+        borderRadius: '5px',
+        // border: 'none',
+        cursor: 'pointer',
+        margin: 10,
+      };
     
   return (
     <div>
         {ingredients && ingredients.map((item) => (
-            <span>{item.name}</span>
+          <button onClick={handleToggle}><span style={isOn?buttonStyle:{fontSize:10}}>{item.name}</span></button>
+          // <button><span style={{fontSize:30}}>{item.name}</span></button>
+            
+    
         ))}
     </div> 
   );
