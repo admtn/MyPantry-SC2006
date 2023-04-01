e6424ec9349fbb3c9e9b67a0e43dbba20db5af29
iejiaa <104296815+iejiaa@users.noreply.github.com>
2023-04-01 22:40:54 +0800

    minor updates

--- Project/my-pantry/src/components/sidebar/Sidebar.jsx
+++ Project/my-pantry/src/components/sidebar/Sidebar.jsx
@@ -1,47 +1,47 @@
 import {
-    ExitToAppOutlined,
-    LocalDiningOutlined,
-    ShoppingBasketOutlined,
-    AccountCircleOutlined,
-  } from "@mui/icons-material";
-  import { useContext } from "react";
-  import { AuthContext } from "../../context/AuthContext";
-  import MenuLink from "../menuLink/MenuLink";
-  import "./sidebar.scss";
-  import { useNavigate } from "react-router-dom";
-  
-  const Sidebar = () => {
-    const { dispatch } = useContext(AuthContext);
-    const navigate = useNavigate();
-  
-    const handleLogout = (e) => {
-      dispatch({ type: "LOGOUT" });
-      navigate("/login");
-    };
+  ExitToAppOutlined,
+  LocalDiningOutlined,
+  ShoppingBasketOutlined,
+  AccountCircleOutlined,
+} from "@mui/icons-material";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import MenuLink from "../menuLink/MenuLink";
+import "./sidebar.scss";
+import { useNavigate } from "react-router-dom";
 
-    
-  
-    return (
-      <div className="sidebar">
-        <div className="sidebarWrapper">
-          <span onClick = { () => { navigate("/pantry")}}>
-          <MenuLink icon={<LocalDiningOutlined/>} text="MyPantry" />
-          </span>
+const Sidebar = () => {
+  const { dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-          <span onClick ={ () => { navigate("/store")}}>
-          <MenuLink icon={<ShoppingBasketOutlined/>} text="Nearby Stores" />
-          </span>
-
-          <span onClick ={ () => { navigate("/")}}>
-          <MenuLink icon={<AccountCircleOutlined/>} text="Profile" />
-          </span>
-
-          <span onClick={handleLogout}>
-            <MenuLink icon={<ExitToAppOutlined/>} text="Logout" />
-          </span>
-        </div>
-      </div>
-    );
+  const handleLogout = (e) => {
+    dispatch({ type: "LOGOUT" });
+    navigate("/login");
   };
+
+  
+
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <span onClick = { () => { navigate("/pantry")}}>
+        <MenuLink icon={<LocalDiningOutlined/>} text="MyPantry" />
+        </span>
+
+        <span onClick ={ () => { navigate("/store")}}>
+        <MenuLink icon={<ShoppingBasketOutlined/>} text="Nearby Stores" />
+        </span>
+
+        <span onClick ={ () => { navigate("/")}}>
+        <MenuLink icon={<AccountCircleOutlined/>} text="Profile" />
+        </span>
+
+        <span onClick={handleLogout}>
+          <MenuLink icon={<ExitToAppOutlined/>} text="Logout" />
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
